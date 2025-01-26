8692d5658c95fd860a4f7b2a0896fb2140c07b0a
G-Moraes <gabrielmoraessilva@outlook.com>
2025-01-26 00:04:01 -0300

    added deploy terraform files

--- infra/setup/main.tf
+++ infra/setup/main.tf
@@ -1,29 +1,29 @@
 terraform {
-	required_providers {
-		aws = {
-			source = "hashicorp/aws"
-			version = "5.23.0"
-		}
-	}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
 
-	backend "s3" {
-		bucket = "gabriel-devops-recipe-app-tf-state"
-		key = "tf-state-setup"
-		region = "us-east-1"
-		encrypt = true
-		dynamodb_table = "gabriel-devops-recipe-app-api-tf-lock"
-	}
+  backend "s3" {
+    bucket         = "gabriel-devops-recipe-app-tf-state"
+    key            = "tf-state-setup"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "gabriel-devops-recipe-app-api-tf-lock"
+  }
 }
 
-provider "aws"{
-	region = "us-east-1"
-	
-	default_tags {
-		tags = {
-			Environment = terraform.workspace
-			Project = var.project
-			contact = var.contact
-			ManageBy = "Terraform/setup"
-		}
-	}
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Project     = var.project
+      contact     = var.contact
+      ManageBy    = "Terraform/setup"
+    }
+  }
 }
--- infra/setup/variables.tf
+++ infra/setup/variables.tf
@@ -1,19 +1,19 @@
 variable "tf_state_bucket" {
- description 	= "Name of S3 bucket in AWS for storing TF state"
- default 			= "gabriel-devops-recipe-app-tf-state"
+  description = "Name of S3 bucket in AWS for storing TF state"
+  default     = "gabriel-devops-recipe-app-tf-state"
 }
 
 variable "tf_state_lock_table" {
-	description = "Name of the DynamoDB table for TF state locking"
-	default = "gabriel-devops-recipe-app-api-tf-lock"
+  description = "Name of the DynamoDB table for TF state locking"
+  default     = "gabriel-devops-recipe-app-api-tf-lock"
 }
 
 variable "project" {
-	description = "Project name for tagging resources"
-	default = "recipe-app-api"
+  description = "Project name for tagging resources"
+  default     = "recipe-app-api"
 }
 
 variable "contact" {
-	description = "Contant name for tagging resources"
-	default = "gabriel@example.com"
+  description = "Contact name for tagging resources"
+  default     = "gabriel@example.com"
 }
